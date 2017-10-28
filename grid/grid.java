e3edcb71ac2c74f9e2dc644a6c57db0a3b219765
SKYNET\Nick Kennedy <nicholas.kennedy1@students.mq.edu.au>
2017-10-29 10:37:46 +1100

    Grid layout along with player array

---
+++ grid/grid.java
@@ -0,0 +1,153 @@
+package grid;
+
+import java.util.Scanner;
+
+public class grid {
+	
+	static String[][] p1shiparray = new String [10][10];
+	static String[][] p1shotsarray = new String [10][10];
+	static String[][] p2shotsarray = new String [10][10];
+	static String[][] p2shipsarray = new String [10][10];
+	static int [] player = {1,2,3,4};	
+	static Scanner KEYBOARD = new Scanner(System.in); 
+	static String text;
+	
+	public static void startvalue(String[][] array){
+		for(int p = 0; p <10; p++){
+			for(int i = 0; i < 10; i++){
+				array[p][i] = "S";
+			}
+		}
+	}
+
+	public static void printships(int s){
+		System.out.println("--------------------------------------------------------------------------------------------");
+		System.out.println("					   Player " + player[s] +"									");
+		System.out.println("	   	 My Ships					MyShots");
+		System.out.println("--------------------------------------------------------------------------------------------");
+		System.out.println("     A   B   C   D   E   F   G   H   I   J  	    A   B   C   D   E   F   G   H   I   J");
+		System.out.print("--------------------------------------------	--------------------------------------------");
+		int i = 0;
+		int p = 0;
+		for(p = 0; p < 10; p++){
+			if(p ==0){
+				System.out.print("\n");
+			}
+			if(p != 9){
+				System.out.print(p + 1 + " ");	
+			}
+			else 
+				System.out.print(p + 1);
+			
+			for(i = 0; i < 10; i++){
+				System.out.print(" | " + p1shiparray[p][i]);
+			}
+			
+			if(i == 10){
+				System.out.print(" |	");
+				
+				if(p != 9){
+					System.out.print(p + 1 + " ");	
+				}
+				
+				else 
+					System.out.print(p + 1);
+				
+				for(int si = 0; si < 10; si++){
+					System.out.print(" | " + p1shotsarray[p][si]);
+				}
+			}
+			
+			System.out.print(" | \n");
+			System.out.println("--------------------------------------------	--------------------------------------------");
+			}
+		
+		keyboad(KEYBOARD); 
+		
+	}
+	
+	public static void keyboad(Scanner scan){ 
+		System.out.print("Enter Coordinates and press Enter to Fire:");
+		text= scan.nextLine(); 
+		validentry(text);
+	}
+	
+	public static void validentry(String s){
+		char colchar= s.charAt(0);
+		String rowchar= s.substring(1);
+		int col = 0;
+		int row = 0;
+
+		if (colchar == 'A' || colchar == 'a' )
+			col = 0;
+		else if (colchar == 'B' || colchar == 'b')
+			col = 1;
+		else if (colchar == 'C' || colchar == 'c')
+			col = 2;
+		else if (colchar == 'D' || colchar == 'd')
+			col = 3;
+		else if (colchar == 'E' || colchar == 'e')
+			col = 4;
+		else if (colchar == 'F' || colchar == 'f')
+			col = 5;
+		else if (colchar == 'G' || colchar == 'g')
+			col = 6;
+		else if (colchar == 'H' || colchar == 'h')
+			col = 7;
+		else if (colchar == 'I' || colchar == 'i')
+			col = 8;
+		else if (colchar == 'J' || colchar == 'j')
+			col = 9;
+		else{
+			System.out.println("Invalid column");
+			keyboad(KEYBOARD);
+		}
+		
+		if (rowchar.contentEquals("1"))
+			row = 0;
+		else if (rowchar.contentEquals("2"))
+			row = 1;	
+		else if (rowchar.contentEquals("3"))
+			row = 2;	
+		else if (rowchar.contentEquals("4"))
+			row = 3;	
+		else if (rowchar.contentEquals("5"))
+			row = 4;	
+		else if (rowchar.contentEquals("6"))
+			row = 5;	
+		else if (rowchar.contentEquals("7"))
+			row = 6;	
+		else if (rowchar.contentEquals("8"))
+			row = 7;	
+		else if (rowchar.contentEquals("9"))
+			row = 8;	
+		else if (rowchar.contentEquals("10"))
+			row = 9;	
+		else{
+			System.out.println("Invalid Row");
+			keyboad(KEYBOARD);
+		}
+		hitcheck(col,row);
+	}
+	
+	public static void hitcheck(int a, int b){
+		if(p1shiparray[b][a].equals("S")){
+			System.out.println("Hit!");
+			p1shiparray[b][a] = "X";
+			printships(0);
+		}
+		else {
+			System.out.println("Miss");
+			p1shiparray[b][a] = "M";
+			printships(0);
+		}
+	}
+
+	public static void main(String[] args) {
+		startvalue(p1shiparray);
+		startvalue(p1shotsarray);
+		printships(0);
+	}
+	
+
+}
