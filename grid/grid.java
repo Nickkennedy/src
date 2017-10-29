1aa36bf8bb893bbdc48f3118991faf051d006c09
Nickkennedy <31816933+Nickkennedy@users.noreply.github.com>
2017-10-29 12:22:08 +1100

    Core Features- Each player has a grid and shots are entered as coordinates submitted for code review along with Bug Fixes
    Completed 2 core features which are each player are assigned a grid and shots are entered in as coordinates. This was also accompanied by a few bug fixes and the never-ending loop for the game's progression. All code and functions are clearly commented.

--- grid/grid.java
+++ grid/grid.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class grid {
 	//Creates the arrays needed for 2 players. 
-	static String[][] p1shiparray = new String [10][10];
+	static String[][] p1shipsarray = new String [10][10];
 	static String[][] p1shotsarray = new String [10][10];
-	static String[][] p2shotsarray = new String [10][10];
 	static String[][] p2shipsarray = new String [10][10];
-	static int [] player = {1,2,3,4};	
+	static String[][] p2shotsarray = new String [10][10];	
 	static Scanner KEYBOARD = new Scanner(System.in); 
-	static String text; //Needed for the keyboard entry
+	static String text; //Used for the keyboard entry
+	static int currentplayer = 1; //Used to monitor which player is currently playing
 	
-	//Sets the starting values of the string arrays
+	//Sets the starting value of " " for all items in the array
 	public static void startvalue(String[][] array){
 		for(int p = 0; p <10; p++){
 			for(int i = 0; i < 10; i++){
@@ -21,54 +21,76 @@ public class grid {
 		}
 	}
 
-	//prints out heads and grids
-	public static void printships(int s){
+	//Prints out the current field for the user passed into the function. Please note that this is still coded to 2 player currently.
+	public static void printships(){
 		System.out.println("--------------------------------------------------------------------------------------------");
-		System.out.println("					   Player " + player[s] +"									");
+		System.out.println("					   Player " + currentplayer +"									");
 		System.out.println("	   	 My Ships					My Shots");
 		System.out.println("--------------------------------------------------------------------------------------------");
 		System.out.println("     A   B   C   D   E   F   G   H   I   J  	    A   B   C   D   E   F   G   H   I   J");
-		System.out.print("--------------------------------------------	--------------------------------------------");
-		int i = 0;
+		System.out.println("--------------------------------------------	--------------------------------------------");
+		// I and P are used for formatting purposes
+		int i = 0; 
 		int p = 0;
+		
+		//PLAYER 1
+		if (currentplayer == 1){
 		for(p = 0; p < 10; p++){
-			if(p ==0){
-				System.out.print("\n");
-			}
 			if(p != 9){
 				System.out.print(p + 1 + " ");	
 			}
 			else 
 				System.out.print(p + 1);
-			
 			for(i = 0; i < 10; i++){
-				System.out.print(" | " + p1shiparray[p][i]);
+				System.out.print(" | " + p1shipsarray[p][i]);
 			}
-			
 			if(i == 10){
 				System.out.print(" |	");
-				
 				if(p != 9){
 					System.out.print(p + 1 + " ");	
 				}
-				
 				else 
 					System.out.print(p + 1);
-				
 				for(int si = 0; si < 10; si++){
 					System.out.print(" | " + p1shotsarray[p][si]);
 				}
-			}
-			
+			}	
 			System.out.print(" | \n");
 			System.out.println("--------------------------------------------	--------------------------------------------");
 			}
+		keyboad(KEYBOARD); //Once printed, allow the user to enter in a keyboard value.
+		}
 		
-		keyboad(KEYBOARD); 
-		
+		// PLAYER 2
+		else if(currentplayer == 2){
+			for(p = 0; p < 10; p++){
+				if(p != 9){
+					System.out.print(p + 1 + " ");	
+				}
+				else 
+					System.out.print(p + 1);
+				for(i = 0; i < 10; i++){
+					System.out.print(" | " + p2shipsarray[p][i]);
+				}
+				if(i == 10){
+					System.out.print(" |	");
+					if(p != 9){
+						System.out.print(p + 1 + " ");	
+					}	
+					else 
+						System.out.print(p + 1);
+					for(int si = 0; si < 10; si++){
+						System.out.print(" | " + p2shotsarray[p][si]);
+					}
+				}
+				System.out.print(" | \n");
+				System.out.println("--------------------------------------------	--------------------------------------------");
+				}
+			keyboad(KEYBOARD); //Once printed, allow the user to enter in a keyboard value.
+		}
 	}
 	
-	//Takes keyboard antry and passes it to be checked
+	//Takes the keyboard entry and passes it to be check if it is valid
 	public static void keyboad(Scanner scan){ 
 		System.out.print("Enter Coordinates and press Enter to Fire:");
 		text= scan.nextLine(); 
@@ -77,8 +99,11 @@ public class grid {
 	
 	//Checks to see if the string entered is valid. If not, returns error
 	public static void validentry(String s){
+		//Converts the keyboard entry into a char (column) and string (row)
 		char colchar= s.charAt(0);
 		String rowchar= s.substring(1);
+		
+		//used to assign values from char/string to int
 		int col = 0;
 		int row = 0;
 
@@ -131,26 +156,56 @@ public class grid {
 			System.out.println("Invalid Row");
 			keyboad(KEYBOARD);
 		}
-		hitcheck(col,row);
+		hitcheck(col,row); //Now the program has the int values of the keyboard entry, pass to check if its a hit
 	}
 	
-	//Now that the keyboard entry is valid, see if its a hit
+	//Once the keyboard entry has been converted to a set of ints, checks if its a hit. Assigns value to array depending on result
 	public static void hitcheck(int a, int b){
-		if(p1shiparray[b][a].equals("S")){
-			System.out.println("Hit!");
-			p1shiparray[b][a] = "X";
-			printships(0);
+		//PLAYER 1
+		if(currentplayer == 1){
+			if(p1shipsarray[b][a].equals("S")){
+				System.out.println("Hit!");
+				p1shotsarray[b][a] = "X";
+				p2shipsarray[b][a] = "X";
+			}
+			else {
+				System.out.println("Miss");
+				p1shotsarray[b][a] = "M";
+				p2shipsarray[b][a] = "M";
+			}
 		}
-		else {
-			System.out.println("Miss");
-			p1shotsarray[b][a] = "M";
-			printships(0);
+		//PLAYER 2
+		else if(currentplayer == 2){
+			if(p2shipsarray[b][a].equals("S")){
+				System.out.println("Hit!");
+				p2shotsarray[b][a] = "X";
+				p1shipsarray[b][a] = "X";
+			}
+			else {
+				System.out.println("Miss");
+				p2shotsarray[b][a] = "M";
+				p1shipsarray[b][a] = "M";
+			}
 		}
 	}
 
 	public static void main(String[] args) {
-		startvalue(p1shiparray);
+		startvalue(p1shipsarray);
 		startvalue(p1shotsarray);
-		printships(0);
+		startvalue(p2shipsarray);
+		startvalue(p2shotsarray);
+		//Endless loop of happiness :)
+		for(int i =0; i < 5; i++){
+			if (currentplayer == 1){
+				printships();
+				currentplayer= 2;
+				i = 0; //Creates never ending loop
+			} 
+			else if(currentplayer == 2){
+				printships();
+				currentplayer= 1;
+				i = 0; //Creates never ending loop
+			}
+		}
 	}
 }
