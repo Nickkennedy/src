e605375f775871ce57689968814b18b5fc543410
s3597881 <31554533+s3597881@users.noreply.github.com>
2017-11-06 20:09:23 +1000

    HandOffScr

--- HandOffScr.java
+++ HandOffScr.java
@@ -5,7 +5,7 @@ public class HandOffScr {
    
    
   public static void printScreenOne(Player p){
-     ///Displays message for players
+     ///Displays message for the next player that the previous has placed all of their ships. Waits for user to press "Enter" to continue 
      
      System.out.println("\n\n\n\n\n\n\t\t\t\t\t =================");
      System.out.println("\t\t\t\t\t NOSRC Battleships");
@@ -15,7 +15,7 @@ public class HandOffScr {
      System.out.println("\n\t\t\t\t      Press \"ENTER\" to continue...");
   }
   public static void printScreenTwo(Player p){
-     ///Displays message for players     
+     ///Displays message for the next player that the previous hashad their turn. Waits for user to press "Enter" to continue    
      
      System.out.println("\n\n\n\n\n\n\t\t\t\t\t=================");
      System.out.println("\t\t\t\t\tNOSRC Battleships");
@@ -25,7 +25,9 @@ public class HandOffScr {
      System.out.println("\n\t\t\t\t  Press \"ENTER\" to continue...");
   }
   public static void promptKey(){
-        
+   
+  ///The method enables to the user to enter the "Enter" keyboard command.
+     
   Scanner scanner = new Scanner(System.in);
   scanner.nextLine();
   scanner.close();     
