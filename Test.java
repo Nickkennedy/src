9b66b360809b9854d041b7c0f4adb200c03467d7
s3597881 <31554533+s3597881@users.noreply.github.com>
2017-11-10 08:07:52 +1000

    Add files via upload
    Test file to see how player and ship runs

---
+++ Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+	public static void main(String[] args) {
+		
+	Player playerOne = new Player("Rob", Ship.CARRIER, Ship.BATTLESHIP, Ship.DESTROYER, Ship.SUBMARINE, Ship.PATROL) {};
+	Player playerTwo = new Player("Bill", Ship.CARRIER, Ship.BATTLESHIP, Ship.DESTROYER, Ship.SUBMARINE, Ship.PATROL) {};
+	
+	System.out.println(playerOne.getPlayerName() );
+	System.out.println(playerTwo.getPlayerName());
+	
+	System.out.println(playerOne.CARRIER.name());
+	System.out.println(playerTwo.BATTLESHIP.name());
+	System.out.println(playerOne.BATTLESHIP.getHitPoints());
+	
+	playerOne.BATTLESHIP.setHitPoints(3);
+	
+	System.out.println(playerOne.BATTLESHIP.getHitPoints());
+	}
+}
