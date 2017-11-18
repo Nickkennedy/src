2a4f8679fd7bccd7953e78bf91afc89b773a8cd4
s3597881 <31554533+s3597881@users.noreply.github.com>
2017-11-18 15:02:33 +1000

    Update HandoffScr.java

--- view/HandoffScr.java
+++ view/HandoffScr.java
@@ -1,4 +1,4 @@
-package view;
+/*package view;
 
 import java.util.Scanner;
 import model.Player;
@@ -11,4 +11,74 @@ public class HandoffScr
       System.out.println("\nHandoff Screen");
       //get keyboard input
    }
+}
+*/
+/*package view;
+
+import java.util.Scanner;
+import model.Player;
+
+
+public class HandoffScr {
+   
+   
+  public static void printScreenOne(Player p){
+     ///Displays message for the next player that the previous has placed all of their ships. Waits for user to press "Enter" to continue 
+     
+     System.out.println("\n\n\n\n\n\n\t\t\t\t\t =================");
+     System.out.println("\t\t\t\t\t NOSRC Battleships");
+     System.out.println("\t\t\t\t\t =================");
+     
+     System.out.println("\n\t\t\t\t"+ p.getPlayerAlias() + "has placed all of their ships");
+     System.out.println("\n\t\t\t\t      Press \"ENTER\" to continue...");
+  }
+  public static void printScreenTwo(Player p){
+     ///Displays message for the next player that the previous has had their turn. Waits for user to press "Enter" to continue.
+     
+     System.out.println("\n\n\n\n\n\n\t\t\t\t\t=================");
+     System.out.println("\t\t\t\t\tNOSRC Battleships");
+     System.out.println("\t\t\t\t\t=================");
+     
+     System.out.println("\n\t\t\t\t" + p.getPlayerAlias() + "  has finished their turn");
+     System.out.println("\n\t\t\t\t  Press \"ENTER\" to continue...");
+  }
+  public static void promptKey(){
+   
+  ///The method enables to the user to enter the "Enter" keyboard command.
+     
+  Scanner scanner = new Scanner(System.in);
+  scanner.nextLine();
+  scanner.close();     
+  }
+}
+*/
+package view;
+
+import java.util.Scanner;
+import model.Player;
+
+
+public class HandoffScr {
+   
+   
+  public HandoffScr(Player last, Player current){
+     ///Displays message for the next player that the previous has placed all of their ships. Waits for user to press "Enter" to continue 
+     
+     System.out.println("\n\n\n\n\n\n\t\t\t\t\t =================");
+     System.out.println("\t\t\t\t\t NOSRC Battleships");
+     System.out.println("\t\t\t\t\t =================");
+     
+     System.out.println("\n\t\t\t\t"+ last.getPlayerAlias() + "has finished their turn...");
+     System.out.println("\n\t\t\t"+ current.getPlayerAlias() + ", Press \"ENTER\" to continue...");
+     
+     promptKey();
+  }
+  public static void promptKey(){
+	   
+  ///The method enables to the user to enter the "Enter" keyboard command.
+     
+  Scanner scanner = new Scanner(System.in);
+  scanner.nextLine();
+  scanner.close();     
+  }
 }
