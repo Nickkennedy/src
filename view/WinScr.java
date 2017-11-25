d7c7ba0c47a470293fea55a3c3774e1c873f5344
s3597881 <31554533+s3597881@users.noreply.github.com>
2017-11-25 15:26:12 +1000

    Update WinScr.java

--- view/WinScr.java
+++ view/WinScr.java
@@ -1,21 +1,3 @@
-/*package view;
-
-import java.util.Scanner;
-
-import model.GameModel;
-import model.Player;
-import view.HOFScr;
-
-public class WinScr 
-{   
-   //constructor.
-   public WinScr(Scanner s, Player p, GameModel m){
-      System.out.println("\n          "+p.getPlayerAlias()+" has Won the game!");
-      new HOFScr(m);
-      //get input here
-      }
-}
-*/
 package view;
 import java.util.Scanner;
 import model.GameModel;
@@ -32,6 +14,11 @@ public class WinScr {
 	
 	public WinScr(Scanner s, Player p, GameModel m) {
 		
+		for(int count=0; count<5; count++) {
+					System.out.println("\n");
+		}
+					
+		
 		System.out.println("\n\n\n\n\n\n\t\t\t\t =================");
 	    System.out.println("\t\t\t\t NOSRC Battleships");
 	    System.out.println("\t\t\t\t =================");
@@ -43,21 +30,26 @@ public class WinScr {
 	    
 	    System.out.println("\n\t\t\t\tTo play again enter 'Y'");
 	    System.out.println("\t\t\t\t To exit enter 'X'");
-	    System.out.print("\t\t\t\t    Selection: ");
 	}
 	
 	/// Waits until the user selects a valid input.
 	public boolean endGameSelection(Scanner s) {
 		
-		String selection;
+		String selection;						
 				
+		System.out.print("\t\t\t\t    Selection: ");
 		selection = s.nextLine().toUpperCase();
 		while(!selection.matches("Y|X")) {
-			selection = s.nextLine().toUpperCase();
+			System.out.println("\t\t\t\tInvalid input try again!!!");
+			System.out.print("\t\t\t\t    Selection: ");
+		 	selection = s.nextLine().toUpperCase();
 		}
-      if(selection.equals("X")) 
-         return true;    //end the game
-      else 
-         return false;   //play another game
+		for(int count=0; count<5; count++) {
+					System.out.println("\n");
+		}
+		if(selection.equals("X")) 
+			return true;    //end the game
+		else 
+			return false;   //play another game
 	}	
 }
